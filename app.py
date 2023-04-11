85ccd38c21f9f4ce2525a5d70294d6fec65be461
Shubhamlad1 <shubhamlad33@gmail.com>
2023-04-11 19:57:50 +0530

    code for teamtoteam

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from ipl import *
 
 app= Flask(__name__)
 
@@ -6,5 +7,14 @@ app= Flask(__name__)
 def home():
     return "Hello World"
 
+
+@app.route('/api/TeamToTeam/stat')
+def TeamStats():
+    team1= request.args.get('team1')
+    team2= request.args.get('team2')
+
+    team_data= TeamToTeam(team1, team2)
+    print(team_data)
+    return jsonify(team_data)
 
 app.run(debug=True, port=7000)
--- ipl.py
+++ ipl.py
@@ -5,3 +5,20 @@ import numpy as np
 ipl_data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv")
 
 
+
+def TeamToTeam(team1, team2): 
+  temp_data= ipl_data[(ipl_data['Team1'] == team1) & (ipl_data['Team2']==team2) | (ipl_data['Team1']== team2) & (ipl_data['Team2']==team1)]
+  Total_Matches_played = temp_data.shape[0]
+  matches_own_by_team1 = temp_data['WinningTeam'].value_counts()[team1]
+  matches_own_by_team2 = temp_data['WinningTeam'].value_counts()[team2]
+
+  draw_Matches = Total_Matches_played - (matches_own_by_team1 - matches_own_by_team2)
+
+  data_dict = {
+      "Total_matched_Played" : str(Total_Matches_played),
+      "matches_Own_by_Team1" : str(matches_own_by_team1),
+      "matches_Own_by_Team2" : str(matches_own_by_team2),
+      "Draw_Matched" : str(draw_Matches)
+  }
+
+  return data_dict
