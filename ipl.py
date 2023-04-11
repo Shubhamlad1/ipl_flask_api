cbdbc7a714a569ae0ed347231972d280fb0d1972
Shubhamlad1 <shubhamlad33@gmail.com>
2023-04-11 18:58:26 +0530

    added data and basic requirements

---
+++ ipl.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
+ipl_data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv")
+
+print(ipl_data)
